04c33d190a5f374cc49f745f38df7830fc71ac5a
Swung0x48 <swung0x48@outlook.com>
2025-03-24 15:10:03 +0800

    [Release] (build.gradle.kts): bump version to 1.2.1. Fix some rendering errors introduced in last version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.fcl.plugin.mobileglues"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1200
-        versionName = "1.2.0"
+        versionCode = 1210
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
