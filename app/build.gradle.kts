3514a4eab93bb5b9459ec6d536ac7bca8372e1fe
BZLZHH <admin@bzlzhh.top>
2025-07-15 23:49:05 +0800

    [Release] (...): Bump version to 1.2.7. Update MobileGlues. Change some text.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.fcl.plugin.mobileglues"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1260
-        versionName = "1.2.6"
+        versionCode = 1270
+        versionName = "1.2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
