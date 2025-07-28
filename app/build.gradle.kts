276ba928b4409e49bcb0abf14ae3231ace1c7eb6
BZLZHH <admin@bzlzhh.top>
2025-07-29 01:25:56 +0800

    [Chore] (MobileGlues): Update MobileGlues to 1.3.0.Alpha.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.fcl.plugin.mobileglues"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1271
-        versionName = "1.2.7 - hotfix1"
+        versionCode = 1299
+        versionName = "1.3.0·Alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
