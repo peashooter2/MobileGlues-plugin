c28c667c04c4304ad063ef7af7f0cd442aa256d5
BZLZHH <admin@bzlzhh.top>
2025-09-20 16:23:58 +0800

    [Chore] (Version): Bump version to 1.3.2.RC2. Update MobileGlues.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1319
-        versionName = "1.3.2·RC1"
+        versionName = "1.3.2·RC2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
