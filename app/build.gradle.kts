f19cfd27552f5c8b50ea6917af6045f4eb8bb6ff
Swung0x48 <swung0x48@outlook.com>
2025-09-22 11:03:18 +0800

    [Chore] (Version): Bump version to 1.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.fcl.plugin.mobileglues"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1319
-        versionName = "1.3.2·RC2"
+        versionCode = 1320
+        versionName = "1.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
